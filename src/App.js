aae22dcfabf813d33883b5068f6387437bce790c
yoyo <timotius_albert@yahoo.com>
2022-01-25 07:31:51 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import dragon from "./download.jpg";
 function App() {
+  const [number1,setNumber1] = useState();
+  const [number2,setNumber2] = useState();
+  const [number,setNumber] = useState();
+
   return (
+    <div id="main">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Number 1 : </h3>
+      <input onChange = {function(data){
+        setNumber1(data.target.value);
+      }} type="number" ></input>
     </div>
+        <div className="App">
+        <h3>Number 2 : </h3>
+        <input onChange = {function(data){
+          setNumber2(data.target.value);
+        }} type="number" ></input>
+      </div>
+      <div className="App">
+      <button onClick = {function(){
+        var random = Math.random();
+        console.log(random);
+        if (random <= 0.5){
+          setNumber(number1);
+        }
+        else {
+          setNumber(number2);
+        }
+      }}>Click</button>
+      </div>
+      <div className="App">
+        {number}
+      </div>
+      </div>
   );
 }
 
